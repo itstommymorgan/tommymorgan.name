bacf865a610d8aaa4b20f0d099e1fb1a213438d5
Tommy Morgan <tommy.morgan@gmail.com>
2020-09-09 18:37:59 +0000

    Middleman it up

---
+++ config.rb
@@ -0,0 +1,20 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Ugly monkeypatching but it seems the only sane way to get around a bug in the
+# autoprefixer logic right now.
+module ::AutoprefixerRails
+  def self.show_deprecation_message!
+    return nil
+  end
+end
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+helpers do
+
+end
