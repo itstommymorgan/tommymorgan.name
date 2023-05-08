5fd1987bcade85c805ea5e83a463462ac13e0bd3
Tommy Morgan <tommy@tommymorgan.name>
2023-05-08 13:02:07 -0400

    ...why do I have to specify the default layout

--- config.rb
+++ config.rb
@@ -20,11 +20,11 @@ page "/*.txt", layout: false
 years = data.slack_statuses.map { |status| status.date.year }.uniq
 
 years.each do |year|
-  proxy "/slack-statuses/#{year}", "/slack.html", locals: { year: year }
+  proxy "/slack-statuses/#{year}", "/slack.html", locals: { year: year }, layout: "layout"
 end
 
 # No idea why I have to hand-code this - will have to figure it out later.
-proxy "/slack-statuses", "/slack.html", locals: { year: 2023 }
+proxy "/slack-statuses", "/slack.html", locals: { year: 2023 }, layout: "layout"
 
 helpers do
 end
